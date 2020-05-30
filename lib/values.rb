07cedb10eaab82b8958013d3a982827b1619536f
Maloochow <luyao.mly@gmail.com>
2020-05-30 18:14:03 -0400

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,9 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  items = []
+  groceries.each do |type, items_array|
+    items = items.concat(items_array)
+  end
+  items
 end
